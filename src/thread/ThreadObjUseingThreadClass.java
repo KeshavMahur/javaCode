9029d678795d6d44a702158cc475b663cae2dde1
keshav <keshav.mahur@insightgeeks.in>
2026-05-21 11:52:41 +0530

    cahanges the msg of thread class

--- src/thread/ThreadObjUseingThreadClass.java
+++ src/thread/ThreadObjUseingThreadClass.java
@@ -4,7 +4,12 @@ public class ThreadObjUseingThreadClass extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("bhati kalu ka chatu employee hai"+Thread.currentThread().getName());
+            System.out.println("Thread Object Created using thread class "+Thread.currentThread().getName());
         }
+    }
+
+    public static void main(String[] args) {
+        ThreadObjUseingThreadClass t1 = new ThreadObjUseingThreadClass();
+        t1.start();
     }
 }
